f7f3f9ea4408c6671b9d2f3f195bcfad3a82fe49
Iain <iain2eyes@gmail.com>
2020-01-08 11:46:59 +0000

    adds conference room basic tests

--- src/main/java/ConferenceRoom.java
+++ src/main/java/ConferenceRoom.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class ConferenceRoom {
+
+    private String name;
+    private int capacity;
+    private ArrayList<Guest> occupants;
+
+    public ConferenceRoom(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+        this.occupants = new ArrayList<Guest>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public int guestCount() {
+        return this.occupants.size();
+    }
 }
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -1,2 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceRoomTest {
+
+    private ConferenceRoom conferenceRoom;
+    private Guest guest;
+
+    @Before
+
+    public void setup(){
+        conferenceRoom = new ConferenceRoom("Ben Nevis", 50);
+        guest = new Guest("Donald");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Ben Nevis", conferenceRoom.getName());
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(50, conferenceRoom.getCapacity());
+    }
+
+    @Test
+    public void occupancyStartsEmpty(){
+        assertEquals(0, conferenceRoom.guestCount());
+    }
 }
