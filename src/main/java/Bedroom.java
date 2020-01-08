7974effbead3a1d5239bc67d45b4d2ca7e26c991
Iain <iain2eyes@gmail.com>
2020-01-08 11:38:16 +0000

    Add basic bedroom attributes

--- src/main/java/Bedroom.java
+++ src/main/java/Bedroom.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+
 public class Bedroom {
+
+    private int roomNumber;
+    private ArrayList<Guest> occupants;
+    private int capacity;
+    private char type;
+
+    public Bedroom(int roomNumber, int capacity, char type) {
+        this.roomNumber = roomNumber;
+        this.occupants = new ArrayList<Guest>();
+        this.capacity = capacity;
+        this.type = type;
+    }
+
+    public int getRoomNumber() {
+        return this.roomNumber;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public char getType() {
+        return this.type;
+    }
+
+
+    public int guestCount() {
+        return this.occupants.size();
+    }
 }
--- src/test/java/BedroomTest.java
+++ src/test/java/BedroomTest.java
@@ -1,2 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BedroomTest {
+
+    private Bedroom bedroom;
+    private Guest guest;
+
+    @Before
+    public void before() {
+        bedroom = new Bedroom(6, 4, 'D');
+        guest = new Guest("Jeremy");
+    }
+
+    @Test
+    public void hasRoomNumber() {
+        assertEquals(6, bedroom.getRoomNumber());
+    }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(4, bedroom.getCapacity());
+    }
+
+    @Test
+    public void hasType() {
+        assertEquals('D', bedroom.getType());
+    }
+
+    @Test
+    public void occupantsStartsEmpty() {
+        assertEquals(0, bedroom.guestCount());
+    }
+
 }
